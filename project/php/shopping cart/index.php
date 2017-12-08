570bc80371444f6a5002ac192b274e39d1b43141
rhapsodist-MK-lenovo <rhapsodist.mk@gmail.com>
2017-12-08 12:23:36 +0900

    shopping cart  - index.php - navbar created

---
+++ project/php/shopping cart/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>mk store</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="#" class="navbar-brand">MK store</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="#"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+                <li><a href="#">Product</a></li>
+                <li style="width:300px; left:10px; top:10px;"><input type="text" class="form-control" id="search_btn"></li>
+                <li style="top:10px; left:20px;"><input type="submit" class="btn btn-primary" id="search_btn"></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user">SignIn</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user">SignUp</a></li>
+            </ul>
+        </div>
+    </div>
+    
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
