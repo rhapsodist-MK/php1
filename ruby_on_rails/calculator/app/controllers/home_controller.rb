f59d5bd6f8c1b78788011a2b81208c07a5ba40d5
rhapsodist-mk <rhapsodist.mk@gmail.com>
2018-06-26 14:42:17 +0900

    backup

--- ruby_on_rails/calculator/app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-	def index
-		
-	end
-
-	def result
-		@plus_result = params[:num1].to_i + params[:num2].to_i
-	end
-end
--- ruby_on_rails/making_page/app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-	def index
-		
-	end
-	def hi
-		@message = "run~!~!~!~!~!~!~!~!"
-		@show_message = true
-	end
-end
